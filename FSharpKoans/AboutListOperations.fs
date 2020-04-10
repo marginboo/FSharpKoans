ccb386bcabd7f36e42c9afa73284ae23bdb3f7ed
Yusuf Motara <cinyc.s@gmail.com>
2020-04-10 16:11:37 +0200

    Fix all URLs (for now - will need to fix them for redirections next year anyway)

--- FSharpKoans/AboutListOperations.fs
+++ FSharpKoans/AboutListOperations.fs
@@ -35,7 +35,7 @@ module ``12: List operations are so easy, you could make them yourself!`` =
         length ["Le Comte de Monte-Cristo"] |> should equal 1
         length [9;3;4;1;6;5;4] |> should equal 7
 
-    // Hint: https://msdn.microsoft.com/en-us/library/ee340354.aspx
+    // Hint: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.length%5b't%5d-function-%5bfsharp%5d
     [<Test>]
     let ``02 Finding the length of a list, the easy way`` () =
         __ [9;8;5;8;45] |> should equal 5
@@ -49,7 +49,7 @@ module ``12: List operations are so easy, you could make them yourself!`` =
         rev [0] |> should equal [0]
         rev [9;3;4;1;6;5;4] |> should equal [4;5;6;1;4;3;9]
 
-    // Hint: https://msdn.microsoft.com/en-us/library/ee340277.aspx
+    // Hint: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.rev%5b't%5d-function-%5bfsharp%5d
     [<Test>]
     let ``04 Reversing a list, the easy way`` () =
         __ [9;8;7] |> should equal [7;8;9]
@@ -94,7 +94,7 @@ module ``12: List operations are so easy, you could make them yourself!`` =
         map ((*) 2) [9;8;7] |> should equal [18;16;14]
         map (fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
 
-    // Hint: https://msdn.microsoft.com/en-us/library/ee370378.aspx
+    // Hint: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.map%5b't,'u%5d-function-%5bfsharp%5d
     [<Test>]
     let ``08 Specified-function mapping, the easy way`` () =
         __ (fun x -> x+1) [9;8;7] |> should equal [10;9;8]
@@ -110,7 +110,7 @@ module ``12: List operations are so easy, you could make them yourself!`` =
         |> should equal ["woof"; "nyan"; "meow"]
         filter (fun (a,b) -> a*b >= 14) [9,3; 4,2; 4,5] |> should equal [9,3; 4,5]
 
-    // Hint: https://msdn.microsoft.com/en-us/library/ee370294.aspx
+    // Hint: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.filter%5b't%5d-function-%5bfsharp%5d
     [<Test>]
     let ``10 Specified-function filtering, the easy way`` () =
         __ (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
@@ -196,7 +196,7 @@ or something else), it's likely that you'll be able to use a fold.
         |> should equal "items: dog cat bat rat"
         fold (fun state item -> state + float item + 0.5) 0.8 [1;3;5;7] |> should equal 18.8
 
-    // Hint: https://msdn.microsoft.com/en-us/library/ee353894.aspx
+    // Hint: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.fold%5b't,'state%5d-function-%5bfsharp%5d
     [<Test>]
     let ``15 Folding, the easy way`` () =
         __ (+) 0 [1;2;3;4] |> should equal 10
@@ -209,7 +209,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``16 exists: finding whether any matching item exists`` () =
         let exists (f : 'a -> bool) (xs : 'a list) : bool =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee370309.aspx
+            __ // Does this: https://msdn.microsoft.com/visualfsharpdocs/conceptual/list.exists%5b%27t%5d-function-%5bfsharp%5d
         exists ((=) 4) [7;6;5;4;5] |> should equal true
         exists (fun x -> String.length x < 4) ["true"; "false"] |> should equal false
         exists (fun _ -> true) [] |> should equal false
@@ -218,7 +218,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``17 partition: splitting a list based on a criterion`` () =
         let partition (f : 'a -> bool) (xs : 'a list) : ('a list) * ('a list) =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353782.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.partition%5b't%5d-function-%5bfsharp%5d
         let a, b = partition (fun x -> x%2=0) [1;2;3;4;5;6;7;8;9;10]
         a |> should equal [2;4;6;8;10]
         b |> should equal [1;3;5;7;9]
@@ -233,7 +233,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``18 init: creating a list based on a size and a function`` () =
         let init (n : int) (f : int -> 'a) : 'a list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee370497.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.init%5b't%5d-function-%5bfsharp%5d
         init 10 (fun x -> x*2) |> should equal [0;2;4;6;8;10;12;14;16;18]
         init 4 (sprintf "(%d)") |> should equal ["(0)";"(1)";"(2)";"(3)"]
 
@@ -241,7 +241,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``19 tryFind: find the first matching element, if any`` () =
         let tryFind (p : 'a -> bool) (xs : 'a list) : 'a option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353506.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.tryfind%5b't%5d-function-%5bfsharp%5d
         tryFind (fun x -> x<=45) [100;85;25;55;6] |> should equal (Some 25)
         tryFind (fun x -> x>450) [100;85;25;55;6] |> should equal None
 
@@ -249,7 +249,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``20 tryPick: find the first matching element, if any, and transform it`` () =
         let tryPick (p : 'a -> 'b option) (xs : 'a list) : 'b option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353814.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.trypick%5b't,'u%5d-function-%5bfsharp%5d
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -279,7 +279,7 @@ or something else), it's likely that you'll be able to use a fold.
         // - why can't it take an 'a->'b, instead of an 'a->'b option ?
         // - why does it return a 'b list, and not a 'b list option ?
         let choose (p : 'a -> 'b option) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353456.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.choose%5b't,'u%5d-function-%5bfsharp%5d
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -297,7 +297,7 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``22 mapi: like map, but passes along an item index as well`` () =
         let mapi (f : int -> 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353425.aspx
+            __ // Does this: https://msdn.microsoft.com/en-us/visualfsharpdocs/conceptual/list.mapi%5b't,'u%5d-function-%5bfsharp%5d
         mapi (fun i x -> -i, x+1) [9;8;7;6] |> should equal [0,10; -1,9; -2,8; -3,7]
         let hailstone i t =
             match i%2 with
